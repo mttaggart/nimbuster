c8647446a67c07a640f6eb9a595cfe7b524899fc
Michael Taggart <michael.t.taggart@gmail.com>
2021-11-10 18:52:32 -0800

    Working multithreaded code

--- src/nimbuster.nim
+++ src/nimbuster.nim
@@ -1,8 +1,14 @@
 # Usage:
 # nimbuster -u --url http://something -w --wordlist wordlist.txt
-import std/parseopt, strformat, httpclient, asyncdispatch
-import strutils
-
+import std/[
+    parseopt, 
+    strformat, 
+    httpclient,
+    threadpool,
+    cpuinfo,
+    strutils,
+    sequtils 
+]
 
 proc usage(): void =
     echo "Usage: "
@@ -10,8 +16,9 @@ proc usage(): void =
 
 # Parse command line URL
 # Parse command line wordlist
-proc parse_args: (string, string, seq[string]) = 
-    result = ("","", @["200","301","302","307","308","401","403","405"])
+proc parse_args: (string, string, int) = 
+    # url, wordlist, threads
+    result = ("","",1)
     var p = initOptParser()
     while true:
         p.next()
@@ -22,34 +29,59 @@ proc parse_args: (string, string, seq[string]) =
                 result[0] = p.val
             if p.key == "w" or p.key == "wordlist":
                 result[1] = p.val
-            if p.key == "s" or p.key == "status-codes":
-                result[2] = p.val.split(",")
+            if p.key == "t" or p.key == "threads":
+                result[2] = parseInt(p.val)
         of cmdArgument:
             continue
 
-proc request(url: string, word: string, status_codes: seq[string]): Future[string] {.async.} = 
-    let client: AsyncHttpClient = newAsyncHttpClient()
-    var r = await client.get(&"{url}/{word}")
-    let status_code = r.status.substr(0,2)
-    result = status_code
-    if status_code in status_codes:
-        echo &"\n{status_code}: {word}"
+proc request(url: string, words: seq[string]) {.thread.} =
+    let status_codes: seq[HttpCode] = @[Http200,Http301,Http302,Http307,Http308,Http401,Http403,Http405]
+    let client: HttpClient = newHttpClient()
+    
+    for w in words:
+        echo &"\r{w}"
+        let status_code = client.get(&"{url}/{w}").code()
+        if status_code in status_codes:
+            echo &"\n{status_code}: {w}"
 
 proc main()  =
-    let (url, wordlist, status_codes) = parse_args()
+    let args = parse_args()
+    let
+        url: string = args[0]
+        wordlist: string = args[1]
+        threads: int = args[2]
+
+    # Exit if url or wordlist not set
     if url == "" or wordlist == "":
         usage()
         return
+
+    let max_threads = countProcessors()
+
+    # Quit if threads not countable
+    if max_threads == 0 and threads <= 0:
+        quit "Could not automatically detect CPU Cores; use --threads", -1
+    elif threads > max_threads:
+        quit fmt"Your machine has a max of {max_threads} threads.", -1
+
+    
     
     # Get the wordlist
+    # Divide it amongst the threads
+    let f: File = open(wordlist)
+    let words: seq[seq[string]] = readAll(f)
+        .splitLines()
+        .filterIt(
+            not(it.startsWith("#")) and it != ""
+        ).distribute(threads)
+    f.close()
+    
+    for t in 1..threads - 1:
+        spawn request(url, words[t])
 
-    let f: File = open(wordlist, fmRead)
-    let words: seq[string] = readAll(f).split("\n")
-    for i in 0..words.len - 1:
-        write(stdout, &"\r{i + 1}/{words.len}")
-        let word: string = words[i]
-        let res = waitFor request(url, word, status_codes)
-        # echo res
+    request(url, words[0])
+
+    sync()
 
 when isMainModule:
     main()
